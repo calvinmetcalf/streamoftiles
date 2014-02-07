fd24d3b9a1b86cbb573c9018d58476923579c61e
Calvin Metcalf <cmetcalf@appgeo.com>
2014-02-07 07:59:10 -0500

    tests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var sm = require('sphericalmercator');
+var SphericalMercator = require('sphericalmercator');
 var Readable = require('readable-stream').Readable;
 var util = require('util');
-
+var sm = new SphericalMercator({
+    size: 256
+});
 util.inherits(TileStream, Readable);
 
 function TileStream(min, max, bbox) {
@@ -12,6 +14,8 @@ function TileStream(min, max, bbox) {
 	this.max = max;
 	this.bbox = bbox;
 	this.z = min - 1;
+	this.bumpZ();
+	this.done = false;
 }
 TileStream.prototype.bumpZ = function(){
 	var xyz = sm.xyz(this.bbox, ++this.z);
@@ -21,6 +25,10 @@ TileStream.prototype.bumpZ = function(){
 	this.maxY = xyz.maxY;
 };
 TileStream.prototype._read = function(){
+	if (this.done) {
+		return;
+	}
+	var self = this;
 	this.curX++;
 	if(this.curX>this.maxX){
 		this.curX=this.minX;
@@ -28,17 +36,19 @@ TileStream.prototype._read = function(){
 		if(this.curY>this.maxY){
 			this.bumpZ();
 			if(this.z>this.max){
+				this.done = true;
 				this.push(null);
 			}
 		}
 	}
-	if(this.push({
+	if(!this.done && this.push({
 		x: this.curX,
 		y: this.curY,
 		z: this.z
 	})){
 		process.nextTick(function(){
-			this._read();
+			self._read();
 		});
 	}
-}
+};
+module.exports = TileStream;
