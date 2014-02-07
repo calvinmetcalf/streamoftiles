8024dcd21f94653f1661dac07594a04cdcbd9021
Calvin Metcalf <cmetcalf@appgeo.com>
2014-02-07 07:41:53 -0500

    typo

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function TileStream(min, max, bbox) {
 }
 TileStream.prototype.bumpZ = function(){
 	var xyz = sm.xyz(this.bbox, ++this.z);
-	this.curX this.minX = xyz.minX;
+	this.curX = this.minX = xyz.minX;
 	this.maxX = xyz.maxX;
 	this.curY = xyz.minY;
 	this.maxY = xyz.maxY;
-}
+};
 TileStream.prototype._read = function(){
 	this.curX++;
 	if(this.curX>this.maxX){
