a8d4d893be87918f104ea54aa2f55d1ce6c41b87
Calvin Metcalf <cmetcalf@appgeo.com>
2017-09-21 08:03:50 -0400

    formating

--- index.js
+++ index.js
@@ -2,57 +2,56 @@ var SphericalMercator = require('sphericalmercator');
 var Readable = require('readable-stream').Readable;
 var util = require('util');
 var sm = new SphericalMercator({
-    size: 256
+  size: 256
 });
 util.inherits(TileStream, Readable);
 
 function TileStream(min, max, bbox) {
   // console.log(min,max, bbox);
-	Readable.call(this, {
-		objectMode: true
-	});
-	this.min = min;
-	this.max = max;
-	this.bbox = bbox;
-	this.z = min - 1;
-	this.bumpZ();
+  Readable.call(this, {
+    objectMode: true
+  });
+  this.min = min;
+  this.max = max;
+  this.bbox = bbox;
+  this.z = min - 1;
+  this.bumpZ();
   // console.log(this.curX, this.curY, this.z);
-	this.done = false;
+  this.done = false;
 }
-TileStream.prototype.bumpZ = function(){
-	var xyz = sm.xyz(this.bbox, ++this.z);
+TileStream.prototype.bumpZ = function() {
+  var xyz = sm.xyz(this.bbox, ++this.z);
   // console.log(xyz);
-	this.curX = this.minX = xyz.minX;
-	this.maxX = xyz.maxX;
-	this.curY = xyz.minY;
-	this.maxY = xyz.maxY;
+  this.curX = this.minX = xyz.minX;
+  this.maxX = xyz.maxX;
+  this.curY = xyz.minY;
+  this.maxY = xyz.maxY;
 };
-TileStream.prototype._read = function(){
-	if (this.done) {
-		return;
-	}
-	var self = this;
-	var flowing = true;
+TileStream.prototype._read = function() {
+  if (this.done) {
+    return;
+  }
+  var flowing = true;
   while (flowing) {
     // console.log(this.curX, this.curY, this.z);
     flowing = this.push({
-  		x: this.curX,
-  		y: this.curY,
-  		z: this.z
-  	});
-  	this.curX++;
-  	if(this.curX>this.maxX){
-  		this.curX=this.minX;
-  		this.curY++;
-  		if(this.curY>this.maxY){
-  			this.bumpZ();
-  			if(this.z>this.max){
-  				this.done = true;
-  				this.push(null);
+      x: this.curX,
+      y: this.curY,
+      z: this.z
+    });
+    this.curX++;
+    if (this.curX > this.maxX) {
+      this.curX = this.minX;
+      this.curY++;
+      if (this.curY > this.maxY) {
+        this.bumpZ();
+        if (this.z > this.max) {
+          this.done = true;
+          this.push(null);
           return;
-  			}
-  		}
-  	}
+        }
+      }
+    }
   }
 };
 module.exports = TileStream;
